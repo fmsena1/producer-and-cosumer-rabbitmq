bb3fdd9fcdc61b6f7aff2c39118a9a540ed99585
Filipe Magalhães Sena <94627887+fmsena1@users.noreply.github.com>
2024-07-08 14:06:23 -0300

    Add project files.

---
+++ consumer-rabbitmq/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "rabbitmq-service",
+    UserName = "user",
+    Password = "password",
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    try
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        
+        Console.WriteLine($" [x] Received {message}");
+        channel.BasicAck(ea.DeliveryTag, false);
+    }
+    catch (Exception ex)
+    {
+        channel.BasicNack(ea.DeliveryTag, false, false);
+    }
+};
+channel.BasicConsume(queue: "hello",
+                     autoAck: false,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
